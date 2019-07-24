511cf6b16945f617c33f5787490fb4e1376049ed
Jianghong Ying <YJianghong@gmail.com>
2019-07-23 20:59:10 -0400

    update Main activity

--- app/src/main/java/com/jianghoy/cybercity2/MainActivity.java
+++ app/src/main/java/com/jianghoy/cybercity2/MainActivity.java
@@ -15,24 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
         ListView eventListView = findViewById(R.id.event_list);
 
-        // what is this adapter constructor means?
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                // what is context???
-                this,
-                R.layout.event_item,
-                R.id.event_name,
-                getEventNames()
-        );
-
-        eventListView.setAdapter(adapter);
+        // below is where we updated
+        eventListView.setAdapter(new EventAdapter(this));
     }
 
-    private String[] getEventNames() {
-        return new String[]{
-                "Event1", "Event2", "Event3",
-                "Event4", "Event5", "Event6",
-                "Event7", "Event8", "Event9",
-                "Event10", "Event11", "Event12"
-        };
-    }
 }
